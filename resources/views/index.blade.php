c4b8aedb75b79b87ec3ea54cd31fcf268eec4918
d.kiselev <mr.dmitrykiselev@gmail.com>
2022-11-11 20:32:39 +0400

    исправление include

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,12 +9,12 @@
 
 @section('container')
     @parent
-    @include('layouts.index_blocks.info');
-    @include('layouts.index_blocks.our_team');
-    @include('layouts.index_blocks.studying_programs');
-    @include('layouts.index_blocks.about');
-    @include('layouts.index_blocks.representatives');
-    @include('layouts.index_blocks.feedback');
+    @include('layouts.index_blocks.info')
+    @include('layouts.index_blocks.our_team')
+    @include('layouts.index_blocks.studying_programs')
+    @include('layouts.index_blocks.about')
+    @include('layouts.index_blocks.representatives')
+    @include('layouts.index_blocks.feedback')
 @endsection
 
 @section('footer')
