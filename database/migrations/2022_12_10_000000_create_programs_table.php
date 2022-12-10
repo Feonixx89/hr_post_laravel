05b03c80c80667e4ebf494cf3f5199426b601845
d.kiselev <d.kiselev@tektorg.ru>
2022-12-10 20:56:55 +0400

    миграции на таблицы программ и состав программ

---
+++ database/migrations/2022_12_10_000000_create_programs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Запустить миграцию.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->boolean('actual')->default(true);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Обратить миграции.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('programs');
+    }
+}
---
+++ database/migrations/2022_12_10_000001_create_compound_programs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompoundProgramsTable extends Migration
+{
+    /**
+     * Запустить миграцию.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compound_programs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('program_id')->references('id')->on('programs');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('duration_in_hours');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Обратить миграции.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('compound_programs');
+    }
+}
