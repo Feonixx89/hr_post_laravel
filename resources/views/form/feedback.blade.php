ff675a771a99f9373670d5b737ebb5f310a5d553
d.kiselev <d.kiselev@tektorg.ru>
2022-12-10 11:19:52 +0400

    fix form + gitignore

--- resources/views/form/feedback.blade.php
+++ resources/views/form/feedback.blade.php
@@ -1,38 +1,23 @@
-<form>
+<form action="{{ route('sendFeedback') }}" method='POST'>
+    @csrf
     <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Имя" aria-label="Username" aria-describedby="basic-addon1"
-               id="username_id" value="123">
+        <input type="text" class="form-control" placeholder="Имя" aria-label="Username" aria-describedby="basic-addon1" name="username">
     </div>
     <div class="input-group mb-3">
-        <input type="text"
-               class="form-control"
-               placeholder="Фамилия"
-               aria-label="UserSurname"
-               aria-describedby="basic-addon2"
-               id="surname_id"
-        >
+        <input type="text" class="form-control" placeholder="Фамилия" aria-label="UserSurname" aria-describedby="basic-addon2" name="surname">
     </div>
     <div class="input-group mb-3">
-        <input type="text"
-               class="form-control"
-               placeholder="Телефон"
-               aria-label="UserPhone"
-               aria-describedby="basic-addon3">
+        <input type="text" class="form-control" placeholder="Телефон" aria-label="UserPhone" aria-describedby="basic-addon3" name="phone">
     </div>
     <div class="input-group mb-3">
-        <input type="email"
-               class="form-control"
-               placeholder="Электронная почта"
-               aria-label="UserEmail"
-               aria-describedby="basic-addon3">
+        <input type="email" class="form-control" placeholder="Электронная почта" aria-label="UserEmail" aria-describedby="basic-addon3" name="email">
     </div>
     <div class="input-group">
-        <textarea class="form-control" aria-label="With textarea" placeholder="Сообщение"></textarea>
+        <textarea class="form-control" aria-label="With textarea" placeholder="Сообщение" name="message"></textarea>
     </div>
     <div class="mb-3 form-check input-group">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <a href="{{ route('policy') }}"> Отправляя форму, вы соглашаетесь с условиями соглашения обработки персональных
-            данных </a>--}}
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="checkPolicy">
+        <a href="{{ route('policy') }}"> Отправляя форму, вы соглашаетесь с условиями соглашения обработки персональных данных </a>--}}
     </div>
-    <button type="submit" class="btn btn-info">Отправить</button>
+    <button  class="btn btn-info">Отправить</button>
 </form>
