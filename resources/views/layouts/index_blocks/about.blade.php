13380387788834a7353f430f0a8b4eec37b329a3
d.kiselev <d.kiselev@tektorg.ru>
2022-12-10 14:39:20 +0400

    about fix

--- resources/views/layouts/index_blocks/about.blade.php
+++ resources/views/layouts/index_blocks/about.blade.php
@@ -1,82 +1,26 @@
-<h1 class="text-center ">О нас говорят</h1>
-<div class="container">
+<div class="container marketing">
+    <h1 class="text-center m-5">О нас говорят</h1>
+
+    <hr class="featurette-divider">
+
     <div class="row">
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+        <div class="col-lg-6">
+            <img src="{{ asset('images/people/about_people/Kyzina_Tatyana.jpg') }}" alt="Татьяна Кузина" width="200"
+                 height="200" class="rounded-circle">
+            <h2>Татьяна Кузина, менеджер по персоналу</h2>
+            <p>"Я прошла обучение в команде профессионалов HR-Post. И просто в восторге от курса! Обучение построено
+                так, что знания нужно сразу применять на практике, и это замечательно! В рамках курса я научилась
+                определять конкурентные преимущества вакансии, строить воронку подбора, грамотно составлять EVP,
+                познакомилась с матрицей омниканального продвижения, с различными источниками найма и многое другое"</p>
         </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+        <div class="col-lg-6">
+            <img src="{{ asset('images/people/about_people/Bazhanova_Elena.jpg') }}" alt="Елена Бажанова" width="200"
+                 height="200" class="rounded-circle">
+            <h2>Елена Бажанова, инспектор отдела кадров</h2>
+            <p>"Хочу выразить искреннюю признательность преподавателям учебного курса «Инструменты эффективного
+                рекрутинга». Все объясняется доступным простым языком со множеством примеров. И каждое новое занятие-это
+                новые знания и наши маленькие победы, может даже над собой. Я получила отличные навыки, новые знакомства
+                с прекрасными людьми и теплоту общения."</p>
         </div>
     </div>
 </div>
-{{--<section class="testimonials" id="testimonials">--}}
-{{--    <div class="row"></div>--}}
-{{--    <div class="title white">--}}
-{{--        <h2 class="titleText">О НАС ГОВОРЯТ</h2>--}}
-{{--    </div>--}}
-{{--    <div class="content">--}}
-{{--        <div class="box">--}}
-{{--            <div class="imgBx">--}}
-{{--                <img src="{{ asset('images/1.jpg') }}">--}}
-{{--            </div>--}}
-{{--            <div class="text">--}}
-{{--                <p>"Я прошла обучение в команде  профессионалов HR-Post. И просто в восторге от курса! Обучение построено так, что знания нужно сразу применять на практике, и это замечательно!--}}
-{{--                    В рамках курса я научилась определять конкурентные преимущества вакансии, строить воронку подбора, грамотно составлять EVP, познакомилась с матрицей омниканального продвижения, с различными источниками найма и многое другое"</p>--}}
-
-{{--            </div>--}}
-{{--            <div class="text">--}}
-{{--                <h3>Татьяна Кузина,<br> менеджер по персоналу</h3>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <div class="box">--}}
-{{--            <div class="imgBx">--}}
-{{--                <img src="{{asset('images/2.jpg')}}">--}}
-{{--            </div>--}}
-{{--            <div class="text">--}}
-{{--                <p>"Хочу выразить искреннюю признательность преподавателям учебного курса «Инструменты эффективного рекрутинга».--}}
-{{--                    Все объясняется доступным простым языком со множеством примеров. И каждое новое занятие-это новые знания и наши маленькие победы, может даже над собой. Я получила отличные навыки, новые знакомства с прекрасными людьми и теплоту общения."--}}
-{{--                </p>--}}
-
-{{--            </div>--}}
-{{--            <div class="text">--}}
-
-{{--                <h3>Елена Бажанова, <br>инспектор отдела кадров</h3>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <div class="box">--}}
-{{--            <div class="imgBx">--}}
-{{--                <img src="{{asset('images/3.webp')}}">--}}
-{{--            </div>--}}
-{{--            <div class="text">--}}
-{{--                <p>"Я прошла обучение в команде  профессионалов HR-Post. "</p>--}}
-
-{{--            </div>--}}
-{{--            <div class="text">--}}
-{{--                <h3>Someone Famous</h3>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</section>--}}
