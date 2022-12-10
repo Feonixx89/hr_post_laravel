4eddd5c3ee08dd440814a0e2fcc98be02b59900b
d.kiselev <d.kiselev@tektorg.ru>
2022-12-10 20:08:12 +0400

    Добавление новых блоков в программы обучения

---
+++ resources/views/layouts/learning_blocks/info.blade.php
@@ -0,0 +1,9 @@
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-1 fw-bold">HR-POST</h1>
+        <h2 class="display-2 fw-bold text-info">Point Of Success Team</h2>
+        <p class="display-5">Точка успеха Вашей команды</p>
+    </div>
+</div>
+
+<hr class="featurette-divider">
