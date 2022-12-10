9a8100d2746453e469007bbd12a68ff4257c1397
d.kiselev <d.kiselev@tektorg.ru>
2022-12-10 11:16:58 +0400

    Добавление JQuery

--- resources/views/layouts/main/main.blade.php
+++ resources/views/layouts/main/main.blade.php
@@ -30,4 +30,5 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
