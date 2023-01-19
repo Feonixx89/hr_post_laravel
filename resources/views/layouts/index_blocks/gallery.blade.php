6525e2d43814c4aacf03b81d38895b7b5010b08e
d.kiselev <d.kiselev@tektorg.ru>
2023-01-20 01:52:37 +0400

    Добавление фотографий в галерею

--- resources/views/layouts/index_blocks/gallery.blade.php
+++ resources/views/layouts/index_blocks/gallery.blade.php
@@ -1,43 +1,70 @@
 <h1 class="text-center m-5">Галерея</h1>
 <div class="row">
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_01.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_01_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/1.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/1.JPG') }}" alt="...">
         </a>
     </div>
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_02.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_02_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/2.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/2.JPG') }}" alt="...">
+        </a>
+    </div>
+
+    <div class="col-md-3 col-sm-4 col-xs-6 thumb">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/5.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/5.JPG') }}" alt="...">
         </a>
     </div>
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_03.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_03_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/6.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/6.JPG') }}" alt="...">
         </a>
     </div>
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_03.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_03_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/7.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/7.JPG') }}" alt="...">
+        </a>
+    </div>
+
+    <div class="col-md-3 col-sm-4 col-xs-6 thumb">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/9.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/9.JPG') }}" alt="...">
         </a>
     </div>
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_02.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_02_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/3.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/3.JPG') }}" alt="...">
         </a>
     </div>
     <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_01.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_01_big.jpg') }}" alt="...">
+        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/4.JPG') }}">
+            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/4.JPG') }}" alt="...">
         </a>
     </div>
-    <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_02.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_02_big.jpg') }}" alt="...">
-        </a>
-    </div>
-    <div class="col-md-3 col-sm-4 col-xs-6 thumb">
-        <a data-fancybox="gallery" href="{{ asset('images/test_gallery/img_03.jpg') }}">
-            <img class="img-fluid" src="{{ asset('images/test_gallery/img_03_big.jpg') }}" alt="...">
-        </a>
-    </div>
+    {{--    <div class="col-md-3 col-sm-4 col-xs-6 thumb">--}}
+    {{--        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/9.JPG') }}">--}}
+    {{--            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/9.JPG') }}" alt="...">--}}
+    {{--        </a>--}}
+    {{--    </div>--}}
+    {{--        <div class="col-md-3 col-sm-4 col-xs-6 thumb">--}}
+    {{--            <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/8.JPG') }}">--}}
+    {{--                <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/8.JPG') }}" alt="...">--}}
+    {{--            </a>--}}
+    {{--        </div>--}}
+    {{--    <div class="col-md-3 col-sm-4 col-xs-6 thumb">--}}
+    {{--        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/10.jpg') }}">--}}
+    {{--            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/10.jpg') }}" alt="...">--}}
+    {{--        </a>--}}
+    {{--    </div>--}}
+    {{--    <div class="col-md-3 col-sm-4 col-xs-6 thumb">--}}
+    {{--        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/11.jpg') }}">--}}
+    {{--            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/11.jpg') }}" alt="...">--}}
+    {{--        </a>--}}
+    {{--    </div>--}}
+    {{--    <div class="col-md-3 col-sm-4 col-xs-6 thumb">--}}
+    {{--        <a data-fancybox="gallery" href="{{ asset('images/photos_gallery/full_size/12.jpg') }}">--}}
+    {{--            <img class="img-fluid" src="{{ asset('images/photos_gallery/full_size/12.jpg') }}" alt="...">--}}
+    {{--        </a>--}}
+    {{--    </div>--}}
 </div>
