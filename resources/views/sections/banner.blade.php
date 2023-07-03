adaf281e09ecedc0a8e3e3b83263cb4526ba048c
d.kiselev <d.kiselev@tektorg.ru>
2023-07-03 12:35:23 +0400

    Исправления текста и картинки

--- resources/views/sections/banner.blade.php
+++ resources/views/sections/banner.blade.php
@@ -7,14 +7,17 @@
                     <div class="hero-content">
                         <h2 class="hero-title mb-35 text-black">HR-POST <br><i>Point Of Success Team</i></h2>
                         <p class="text-black">Точка успеха Вашей команды.</p>
-{{--                        <div class="tp-banner-btn">--}}
-{{--                            <a href="course-list.html" class="tp-btn">Наши интенсивы</a>--}}
-{{--                        </div>--}}
                     </div>
                 </div>
                 <div class="banner-shape d-none d-lg-block">
                     <img src="{{ asset('/assets/img/banner-img-1.png') }}" alt="banner-shape" class="b-shape"
                          height="925" width="827">
+                </div>
+            </div>
+            <div class="row rounded float-end d-lg-none d-xl-none d-xxl-none">
+                <div class="col-xl-12 col-lg-6 col-md-8 ">
+                    <img src="{{ asset('/assets/img/banner-img-1.png') }}" alt="banner-shape"
+                         height="500" width="400">
                 </div>
             </div>
         </div>
